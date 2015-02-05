b170a99ed6d104db1ed4e4724dc986d91477f7b2
Bao Vuong <vqbao@lv5.org>
2015-02-04 23:06:47 -0500

    arrays.cpp can read from input_data.txt, print it to the console, and then save it into output_data.txt

--- arrays.cpp
+++ arrays.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <fstream> // for ifstream
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    // closed arrays
     
-    double numbers[] = {1,2,3,4};
+    ifstream inFile;
+    vector<int> numbers;
+    inFile.open("input_data.txt");
     
+    if (inFile.is_open()) {
+        // ok, it's open, time to read the data
+        string line;
+        int number;
+        while (getline(inFile,line)) {
+            string::size_type sz;
+            number = stoi(line,&sz);
+            numbers.push_back(number);
+        }
+        inFile.close();
+    }
+    
+    // printing out the array of numbers
+    for (unsigned int i=0; i<numbers.size(); i++) {
+        cout << numbers[i] << " ";
+    }
+    
+    // writing to file
+    ofstream outFile;
+    outFile.open("output_data.txt");
+    for (unsigned int i=0; i<numbers.size(); i++) {
+        outFile << i << ": " << numbers[i] << endl;
+    }
+    outFile.close();
+    cout << endl;
     return 0;
 }
